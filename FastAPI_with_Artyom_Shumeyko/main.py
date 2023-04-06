0627c0b34fc3fc2ece4ed4188df44c79c7b2a7c9
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-04-07 02:23:34 +0500

    first commit

---
+++ FastAPI_with_Artyom_Shumeyko/main.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
+from fastapi_users import FastAPIUsers
+from fastapi import FastAPI, Request, status, Depends
+from pydantic import BaseModel, Field
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import ValidationError
+from fastapi.responses import JSONResponse
+from auth.database import User
+from auth.auth import auth_backend
+from auth.manager import get_user_manager
+from auth.schemas import UserRead, UserCreate
+
+app = FastAPI(
+    title="Trading App"
+)
+
+
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend]
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix='/auth',
+    tags=["auth"],
+)
+
+current_user = fastapi_users.current_user()
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
