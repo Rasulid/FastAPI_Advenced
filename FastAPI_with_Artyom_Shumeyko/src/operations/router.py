6377ae7a5e2d3b61be0b344c216652b11f0a4fb6
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-04-09 16:57:17 +0500

    8 lesson redis

--- FastAPI_with_Artyom_Shumeyko/src/operations/router.py
+++ FastAPI_with_Artyom_Shumeyko/src/operations/router.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends
+import time
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi_cache.decorator import cache
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
-
+from typing import Dict, List
 from database import get_async_session
 from operations.model import operation
 from operations.schemas import OperationCreate
@@ -10,12 +13,27 @@ router = APIRouter(tags=['Operations'],
                    prefix='/operations')
 
 
-@router.get("/")
+@router.get('long_operation')
+@cache(expire=30)
+def long_operation():
+    time.sleep(2)
+    return "So many data"
+
+
+@router.get("/", response_model=List[OperationCreate])
 async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
-    query = select(operation).where(operation.c.type == operation_type)
-    result = await session.execute(query)
-    print(result)
-    return result.scalars().all()
+    try:
+        query = select(operation).where(operation.c.type == operation_type)
+        result = await session.execute(query)
+        # return {'status': "success",
+        #         "data": result.all(),
+        #         "details": None}
+        return result.all()
+    except Exception:
+        raise HTTPException(status_code=499, detail={
+                'status': "error",
+                "data": None,
+                "details": None})
 
 
 @router.post('/')
@@ -24,4 +42,5 @@ async def add_specific_operations(new_operation: OperationCreate,
     stmt = insert(operation).values(**new_operation.dict())
     result = await session.execute(stmt)
     await session.commit()
+
     return {"status": "success"}
