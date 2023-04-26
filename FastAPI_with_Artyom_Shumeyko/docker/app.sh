c61064b4d375a2fd5a792383d99394ccad9bd11f
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-04-26 09:47:52 +0500

    finish

--- FastAPI_with_Artyom_Shumeyko/docker/app.sh
+++ FastAPI_with_Artyom_Shumeyko/docker/app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-alembic update head
+alembic upgrade head
 
 cd src
 
-gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8008
+gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000
--- FastAPI_with_Artyom_Shumeyko/src/config.py
+++ FastAPI_with_Artyom_Shumeyko/src/config.py
@@ -13,6 +13,7 @@ REDIS_HOST = os.environ.get("REDIS_HOST")
 REDIS_PORT = os.environ.get("REDIS_PORT")
 
 
+
 DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
 DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
 DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
